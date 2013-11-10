18f6201a68eb63c89e96b8d6f30cb4e1287bf6a6
Mariano Marin <mdm096@utulsa.edu>
2013-11-10 12:10:44 -0600

    question manager commented out

--- TicTacServe/src/game/QuestionManager.java
+++ TicTacServe/src/game/QuestionManager.java
@@ -1,26 +1,26 @@
-package game;
-import java.util.ArrayList;
-import java.util.Map;
-
-import com.google.*;
-import com.google.gson.*;
-import com.google.gson.annotations.*;
-import java.util.Random;
-
-public class QuestionManager {
-	private ArrayList<Question> questions;
-	private Random randomizer;
-	
-	public QuestionManager(){
-		//TODO get all the questions from the database
-	}
-	
-	public QuestionManager(int numQuests){
-		//TODO get the specified number of questions
-	}
-	
-	public Question getUnansweredQuestion(){
-		
-		return null;
-	}
-}
+//package game;
+//import java.util.ArrayList;
+//import java.util.Map;
+//
+//import com.google.*;
+//import com.google.gson.*;
+//import com.google.gson.annotations.*;
+//import java.util.Random;
+//
+//public class QuestionManager {
+//	private ArrayList<Question> questions;
+//	private Random randomizer;
+//	
+//	public QuestionManager(){
+//		//TODO get all the questions from the database
+//	}
+//	
+//	public QuestionManager(int numQuests){
+//		//TODO get the specified number of questions
+//	}
+//	
+//	public Question getUnansweredQuestion(){
+//		
+//		return null;
+//	}
+//}
