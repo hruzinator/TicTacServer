8330c336e050984b83bf078aad0eb0fc768ee52d
Matthew Hruz <matthew.hruz@gmail.com>
2013-11-10 01:13:17 -0600

    Question and QuestionManager functionality
    Added full functionality to Question (unless we plan on adding anything
    to it later), and functionality for QuestionManager. I have not tested
    readFromJSON file yet. We will have to check on that.

--- TicTacServe/src/Question.java
+++ TicTacServe/src/Question.java
@@ -1,5 +1,26 @@
-//mariano's comment
 public class Question 
 {
-	int questionNum = 0;
+	private int questionNum = 0;
+	private String question;
+	private String answer;
+	boolean isAnswered = false;
+	boolean isTried = false;
+	
+	public Question(int qNum, String q, String a) {
+		super();
+		this.questionNum = qNum;
+		this.question = q;
+		this.answer = a;
+	}
+	
+	boolean attemptAnswer(String a){
+		isTried = true;
+		if(a.equals(answer)){
+			isAnswered = true;
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 }
--- TicTacServe/src/QuestionManager.java
+++ TicTacServe/src/QuestionManager.java
@@ -1,4 +1,32 @@
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.google.*;
+import com.google.gson.*;
+import com.google.gson.annotations.*;
 
 public class QuestionManager {
-
+	ArrayList<Question> questions;
+		
+	public Question getUnansweredQuestion(){
+		
+		return null;
+	}
+	
+	private void readFromJSONFile(String jsonString){
+		Gson gson = new Gson();
+		Response response = gson.fromJson(jsonString, Response.class);
+	}
+	
+	private class Response {
+		  private Map<String, App> descriptor;
+		  //getters&setters
+		}
+	
+	private class App {
+		  private String name;
+		  private int age;
+		  private String[] messages;
+		  //getters&setters
+		}
 }
